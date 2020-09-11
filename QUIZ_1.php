b060d60809adb2e9f4b01fee04bbcd82978874e8
lawrencepytleski2 <59966826+lawrencepytleski2@users.noreply.github.com>
2020-09-11 02:32:48 -0400

    Add files via upload

---
+++ QUIZ_1.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * q1.txt
+ *
+ * Quiz 1 regarding HTML and CSS
+ *
+ * @category   Quiz
+ * @package    Quiz 1
+ * @author     Lawrence Pytleski
+ * @version    2020.09.10
+ * @link       ljpytleski@hawkmail.hfcc.edu
+ */
+
+// 3 pts
+// 1. Create a basic HTML 5 page frame below.
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>QUIZ_1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+        aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+        non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.
+    </div>
+</body>
+</html>
+
+// 2 pts
+// 2. Add a link tag to the HTML frame above, and name the file style.css
+// (this file does not need to really exist for this quiz)
+
+
+// 2 pts
+// 3. Add a div tag to the body of the page above, and fill it with any content you wish
+// Lorum ipsum is recommended
+
+
+// 1 pt
+// 4. Create a PHP variable named myVar and assign any string into it.
+
+$myVar = 'Bob';
+
+// 1 pt
+// 5. Define inheritance below.
+
+Inheritance is the process by which properties
+are passed from parent to child elements even
+though those properties have not been explicitly
+defined by other means.
+// 1 pt
+// 6. What are the programs you intend to use for this class? List them below.
+
+phpstorm,
+filezilla,
+possibly notepad++,
+and I might try something new...
+
+
+
+
+
+?>
